26eb85676faa03c3dc28eb8e944a99a4ab0f8e71
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2021-04-19 00:57:58 -0500

    Añadiendo socket temperatura prueba-1

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
 from websocket import create_connection
 import json
-import RPi.GPIO as GPIO
 from Dispositivos import Dispositivo
+from Sensores import Sensores
+import RPi.GPIO as GPIO 
 
 def conexion_adonis():
-    ws = create_connection("ws://54.146.120.131:3333/adonis-ws")
+    data={'sensor': "TEMPERATURA_HUMEDAD",'tipo':"DHT11",'pin': 14} #Diccionario del sensor temperartura
+    GPIO.setmode(GPIO.BCM)
+    ws = create_connection("ws://54.146.120.131:3333/adonis-ws") #Se enlaza al socket
 
     ws.send(json.dumps({ #me uno al canal
         "t":1,
@@ -14,30 +17,24 @@ def conexion_adonis():
     }))
     print("respuesta al conectarme al canal")
     print(ws.recv())
-    ws.send(json.dumps({ #despues realizo un evento en el canal
-        "t":7,
-        "d": {
-            "topic":"wstemp",
-            "event":"message",
-            "data":"Hola aws"
-        }
-    }))
-    print("respuesta al conectarme al channel y enviar un mensaje")
-    print(ws.recv())
+    """ print("respuesta al conectarme al channel y enviar un mensaje")
+    print(ws.recv()) """
 
-    print("recibiendo mensajes...")
+    print("Listo para enviar temperatura...")
     try:
-        
         while True:
-            result = ws.recv()
-            result = json.loads(result)
-            print ("Recibidos: ")
-            data = result['d']['data']
-            print(data)
+            sensor = Sensores(data)
+            print("Temperatura registrada: "+sensor['message']['temperatura'])
+            ws.send(json.dumps({ #despues realizo un evento en el canal, es decir envio en un mensaje
+                "t":7,
+                "d": {
+                    "topic":"wstemp",
+                    "event":"message",
+                    "data":sensor['message']['temperatura']
+                }
+            }))
             #Foco = Dispositivo()
         
-
-
     except: 
         print("conexion perdida")
         conexion_adonis()
