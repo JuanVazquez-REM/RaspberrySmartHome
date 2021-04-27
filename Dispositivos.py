7a54c1813630f7a6ca8a0321b10383a1d676dbb6
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2021-04-27 16:33:15 -0500

    M

--- Dispositivos.py
+++ Dispositivos.py
@@ -12,10 +12,12 @@ class Dispositivo:
             self.offDevice(data)
 
     def onDevice(self,data):
+        GPIO.setup(data['pin'], GPIO.OUT)
         GPIO.output(data['pin'],False)
         return response
 
     def offDevice(self,data):
+        GPIO.setup(data['pin'], GPIO.OUT)
         GPIO.output(data['pin'],True)
         return response
 
