467a7d19a5516b9d1573436d3a6b6106466930ab
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2021-04-27 03:24:28 -0500

    Modificando dispositivos

--- Dispositivos.py
+++ Dispositivos.py
@@ -5,6 +5,7 @@ response = {'status': True, 'message': 'Accion realizada'}
 class Dispositivo:
 
     def __init__(self,data): # data=nombre,estado,pin
+        GPIO.setmode(GPIO.BCM)
         if(data['estado'] == 1):
             self.onDevice(data)
         elif(data['estado']== 0):
