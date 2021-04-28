421e97e7c89b0e2363afa19e72591309fb15b922
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2021-04-28 06:39:04 -0500

    Dq

--- Dispositivos.py
+++ Dispositivos.py
@@ -14,7 +14,7 @@ class Dispositivo:
     def onDevice(self,data):
         if(data['in'] == 1):
             GPIO.setup(data['pin'], GPIO.OUT)
-            GPIO.output(data['pin'],False)
+            GPIO.output(data['pin'],True)
         else:
             GPIO.setup(data['pin'], GPIO.OUT)
             GPIO.output(data['pin'],True)
@@ -26,7 +26,7 @@ class Dispositivo:
             GPIO.output(data['pin'],False)
         else:
             GPIO.setup(data['pin'], GPIO.OUT)
-            GPIO.output(data['pin'],True)
+            GPIO.output(data['pin'],False)
         return response
 
 
