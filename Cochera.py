4786616ddd0c624835bfe3727db36481e245a699
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2021-04-28 03:54:21 -0500

    añadiendo libreria dht

---
+++ Cochera.py
@@ -0,0 +1,52 @@
+from websocket import create_connection
+import json
+from Dispositivos import Dispositivo
+import RPi.GPIO as GPIO
+
+
+def conexion_adonis():
+    ws = create_connection("ws://54.146.120.131:3333/adonis-ws") #Se enlaza al socket
+
+    ws.send(json.dumps({ #me uno al canal
+        "t":1,
+        "d": {
+            "topic":"wscochera",
+        }
+    }))
+
+    print("respuesta al conectarme al canal")
+    print(ws.recv())
+
+    ws.send(json.dumps({ #despues realizo un evento en el canal, es decir envio en un mensaje
+        "t":7,
+        "d": {
+            "topic":"wscochera",
+            "event":"message"
+        }
+    }))
+    print("Respesta despues de enviar mensaje")
+    print(ws.recv())
+
+
+    #print("Listo para enviar temperatura...")
+    try:
+        while True:
+            print("Esperando accion cochera...")
+            messageJson = ws.recv()
+            print(messageJson)
+            messageDecoder = json.loads(messageJson)
+            data = messageDecoder['d']['data'] 
+
+            cochera= Dispositivo(data)
+            print(cochera)
+
+            print("Accion completada")
+    except: 
+        print("conexion perdida")
+        conexion_adonis()
+        print("Estableciendo conexion...")
+    finally:
+        GPIO.cleanup()  
+
+conexion_adonis()
+
