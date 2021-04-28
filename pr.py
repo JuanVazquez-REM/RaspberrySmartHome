b4349de4822eb9c5c3745541bfa7ca96a2411bbc
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2021-04-28 08:30:23 -0500

    Dqwdassa

--- pr.py
+++ pr.py
@@ -8,10 +8,10 @@ GPIO.setup(servoPIN, GPIO.OUT)
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(0.1) 
 try:
-    while True:
-        p.ChangeDutyCycle(10)
-        time.sleep(2)
-        p.ChangeDutyCycle(1)
+    
+    p.ChangeDutyCycle(20)
+    time.sleep(2)
+    p.ChangeDutyCycle(1)
         
 except KeyboardInterrupt:
     p.stop()
