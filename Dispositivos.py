28b6a756ecc69087b8592c912eab63e9eb499211
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2021-04-28 06:51:42 -0500

    Dqwdas

--- Dispositivos.py
+++ Dispositivos.py
@@ -6,27 +6,25 @@ class Dispositivo:
 
     def __init__(self,data): # data=nombre,estado,pin
         GPIO.setmode(GPIO.BCM)
-        if(data['estado'] == 1):
-            self.onDevice(data)
-        elif(data['estado']== 0):
-            self.offDevice(data)
+        if(data['in']== 1):
+            if(data['estado'] == 0):
+                self.onDevice(data)
+            elif(data['estado']== 1):
+                self.offDevice(data)
+        else:
+            if(data['estado'] == 1):
+                self.onDevice(data)
+            elif(data['estado']== 0):
+                self.offDevice(data)
 
     def onDevice(self,data):
-        if(data['in'] == 1):
-            GPIO.setup(data['pin'], GPIO.OUT)
-            GPIO.output(data['pin'],False)
-        else:
-            GPIO.setup(data['pin'], GPIO.OUT)
-            GPIO.output(data['pin'],True)
+        GPIO.setup(data['pin'], GPIO.OUT)
+        GPIO.output(data['pin'],True)
         return response
 
     def offDevice(self,data):
-        if(data['in'] == 1):
-            GPIO.setup(data['pin'], GPIO.OUT)
-            GPIO.output(data['pin'],True)
-        else:
-            GPIO.setup(data['pin'], GPIO.OUT)
-            GPIO.output(data['pin'],False)
+        GPIO.setup(data['pin'], GPIO.OUT)
+        GPIO.output(data['pin'],False)
         return response
 
 
